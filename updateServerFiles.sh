9912cc446c9dbd30808cd6481d688080cb327654
Pamir Ghimire <pamirghimire@gmail.com>
2020-10-17 10:07:04 -0700

    add buttons and output text box plus css

--- updateServerFiles.sh
+++ updateServerFiles.sh
@@ -1,3 +1,5 @@
 sudo rm -rf /var/www/simpleWebsite.com
 sudo mkdir /var/www/simpleWebsite.com
-sudo cp ./index.html /var/www/simpleWebsite.com/
+
+sudo cp ./*.html /var/www/simpleWebsite.com/
+sudo cp ./*.css /var/www/simpleWebsite.com/
