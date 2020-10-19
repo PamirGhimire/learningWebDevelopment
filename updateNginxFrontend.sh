af578f84956d87fdf93efd8f1f2bd4a1f19327ac
Pamir Ghimire <pamirghimire@gmail.com>
2020-10-18 23:33:56 -0700

    update nginx frontend update

---
+++ updateNginxFrontend.sh
@@ -0,0 +1,6 @@
+sudo rm -rf /var/www/simpleWebsite.com
+sudo mkdir /var/www/simpleWebsite.com
+
+sudo cp ./frontend/*.html /var/www/simpleWebsite.com/
+sudo cp ./frontend/*.css /var/www/simpleWebsite.com/
+sudo cp ./frontend/*.js /var/www/simpleWebsite.com/
--- updateServerFiles.sh
+++
@@ -1,6 +0,0 @@
-sudo rm -rf /var/www/simpleWebsite.com
-sudo mkdir /var/www/simpleWebsite.com
-
-sudo cp ./*.html /var/www/simpleWebsite.com/
-sudo cp ./*.css /var/www/simpleWebsite.com/
-sudo cp ./*.js /var/www/simpleWebsite.com/
