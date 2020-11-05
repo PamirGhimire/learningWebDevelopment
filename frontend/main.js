f5fa767e7fd124a8b6b855b3993a64480095898c
Pamir Ghimire <pamirghimire@gmail.com>
2020-11-05 06:26:22 -0800

    capture and display av data in browser

--- frontend/main.js
+++ frontend/main.js
@@ -1,47 +1,14 @@
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-let userId = makeid(5);
-document.getElementById("sendMessage").addEventListener("click", SendMessage);
 
-// get chathistory from backend periodically and update the chatbox
-setInterval(() =>
-{
-    xhr = new XMLHttpRequest(); 
-    var url = "http://localhost:3000/chatHistory"; 
-    xhr.open("GET", url, true); 
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      const data = xhr.response;  
-      document.getElementById("conversation").innerHTML = data["chatHistory"]
-    };
-
-    xhr.send();
-}, 1000);
-
-
-function SendMessage()
-{
-    var newMessage = document.getElementById("newMessage").value;
-
-    xhr = new XMLHttpRequest(); 
-    var url = "http://localhost:3000/newMessage"; 
-    xhr.open("POST", url, true); 
-    xhr.setRequestHeader("Content-type", "application/json"); 
-    xhr.onreadystatechange = function () {  
-        if (xhr.readyState == 4 && xhr.status == 200) { 
-            var json = JSON.parse(xhr.responseText); 
-            console.log(json.email + ", " + json.name) 
-        } 
-    } 
-    var data = JSON.stringify({"userId": String(userId), "newMessage": newMessage}); 
-    xhr.send(data);
-
-    document.getElementById("newMessage").value = "";
-}
+(function () {
+    "use strict";
+  
+    document.addEventListener('click', async event => {
+      if (event.target.id === 'button') {
+        const stream = await window.navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        const video = document.getElementById('video');
+        video.srcObject = stream;
+        video.play();
+      }
+    });
+  
+  })();
